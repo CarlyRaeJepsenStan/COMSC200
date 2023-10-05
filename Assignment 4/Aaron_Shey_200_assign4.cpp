6b660faa284e99d8e7636d61f0a79beba2033964
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-10-05 07:33:24 -0700

    cleaning up

--- Assignment 4/Aaron_Shey_200_assign4.cpp
+++ Assignment 4/Aaron_Shey_200_assign4.cpp
@@ -1,5 +1,6 @@
 #include "Aaron_Shey_HeartRates.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -7,5 +8,15 @@ int main() {
        << "\n";
   string input;
   getline(cin, input);
-  input
+  int index = input.find(' ');
+  string first = input.substr(0, index);
+  string last = input.substr(index + 1, input.length() - index);
+
+  cout << "Enter the date in Month Day Year format, separated by spaces: "
+       << "\n";
+  string input2;
+  getline(cin, input2);
+  vector<int> date;
+  for (char x : input2) {
+  }
 }
