105cab25fbb55ba60bab8bece47a59340d4d1b18
CarlyRaeJepsenStan <44917034+CarlyRaeJepsenStan@users.noreply.github.com>
2023-10-24 10:00:18 -0700

    added stuff

--- Assignment 7/Aaron_Shey_IntegerSet.cpp
+++ Assignment 7/Aaron_Shey_IntegerSet.cpp
@@ -9,6 +9,27 @@ IntegerSet::IntegerSet() : internal(vector<bool>(100, false)){};
 IntegerSet::IntegerSet(int array[], int size) {
   vector<bool> internal(100, false);
   for (int i = 0; i < size; i++) {
+    internal[array[i]] = true;
   }
+}
+
+vector<bool> IntegerSet::unionOfSets(IntegerSet in) {
+  vector<bool> out(100, false);
+  for (int i = 0; i < 100; i++) {
+    if (this[i] == in[i] && (this[i]==true || in[i] == true)) {
+      out[i]=true;
+    }
+  }
+  return out;
+}
+vector<bool> IntegerSet::intersectionOfSets(IntegerSet in) {
+  vector<bool> out(100, false);
+
+
+}
+
+vector<bool> IntegerSet::inputSet() {
+  int u; 
+  
 }
 #endif
