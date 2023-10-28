b86751ecbbc8adfd2736500babe42db7a7adb98d
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-10-28 13:39:16 -0700

    Finally done

--- Assignment 7/Aaron_Shey_IntegerSet.cpp
+++ Assignment 7/Aaron_Shey_IntegerSet.cpp
@@ -4,15 +4,31 @@
 using namespace std;
 #ifndef INTEGERSET_H
 #define INTEGERSET_H
+// 3.  I also added this helper function, to make my code more concise by
+// replacing repeated lines.
+bool validate(int in) {
+  bool out = false;
+  if (0 <= in && in <= 100) {
+    out = true;
+  }
+  return out;
+}
 
 IntegerSet IntegerSet::operator=(const IntegerSet i) { return *this; }
 
+bool IntegerSet::operator[](int i) { return internal[i]; }
+
+// 4.  I made each IntegerSet 101 elements long, because the numbers from 0 to
+// 100 have
 IntegerSet::IntegerSet() : internal(vector<bool>(101, false)){};
 
 IntegerSet::IntegerSet(int array[], int size)
     : internal(vector<bool>(101, false)) {
   for (int i = 0; i < size; i++) {
-    if (array[i] <= 100 && array[i] >= 0) {
+    // 5.  This is where I encountered my first error!  My code was failing to
+    // properly handle negative elements, even though I had the validate
+    // function in it already.  I'm not sure what fixed it, but it went away.'
+    if (validate(array[i])) {
       internal[array[i]] = true;
     } else {
       cout << "Invalid insert attempted!\n";
@@ -20,18 +36,43 @@ IntegerSet::IntegerSet(int array[], int size)
   }
 }
 
+IntegerSet IntegerSet::inputSet() {
+  IntegerSet out;
+  int u;
+  while (true) {
+    cout << "Enter a number (-1 to end ): ";
+    cin >> u;
+    if (u < 0) {
+      break;
+    }
+    if (validate(u)) {
+      out.insertElement(u);
+    } else {
+      cout << "Invalid input. Enter a number between 0 and 99." << endl;
+    }
+  }
+  cout << "Entry complete\n";
+  return out;
+}
+
 IntegerSet IntegerSet::unionOfSets(IntegerSet in) {
   IntegerSet out;
   for (int i = 0; i < 101; i++) {
+    // 6.  I am particularly proud of this line - it depends on C++'s built-in
+    // truthiness to ensure that both elements are true.  Its concision and
+    // cleverness are unmatched.
     if (internal[i] && in[i]) {
       out.insertElement(i);
     }
   }
   return out;
 }
+
 IntegerSet IntegerSet::intersectionOfSets(IntegerSet in) {
   IntegerSet out;
   for (int i = 0; i < 101; i++) {
+    // 7.  I also found this line to be particularly clean and concise, similar
+    // to my other comment above.
     if (internal[i] || in[i]) {
       out.insertElement(i);
     }
@@ -39,34 +80,27 @@ IntegerSet IntegerSet::intersectionOfSets(IntegerSet in) {
   return out;
 }
 
-IntegerSet IntegerSet::inputSet() {
-  IntegerSet out;
-  int u;
-  while (true) {
-    cout << "Enter a number (negative to stop): ";
-    cin >> u;
-    if (u < 0) {
-      break;
-    }
-    if (u >= 0 && u <= 100) {
-      out.insertElement(u);
-    } else {
-      cout << "Invalid input. Enter a number between 0 and 99." << endl;
-    }
-  }
-  return out;
-}
-
+// 8.  I found this function to be rather redundant; vectors already have a
+// equality operator.
 bool IntegerSet::isEqualTo(IntegerSet in) { return (internal == in.internal); }
 
-void IntegerSet::insertElement(int m) { internal[m] = true; }
+// 9.  These two functions were very similar.  I wonder if there is a way to
+// combine theme to make my code more concise.
+void IntegerSet::insertElement(int m) {
+  if (validate(m)) {
+    internal[m] = true;
+  }
+}
 
 void IntegerSet::deleteElement(int k) {
-  if (k >= 0 && k <= 100) {
+  if (validate(k)) {
     internal[k] = false;
   }
 }
 
+// 10.  I was contemplating using this->internal syntax for most of my
+// functions, but decided not to.  This was probably the easiest function to
+// write.
 void IntegerSet::printSet() {
   cout << "{ ";
   for (int i = 0; i < 101; i++) {
--- Assignment 7/Aaron_Shey_IntegerSet.h
+++ Assignment 7/Aaron_Shey_IntegerSet.h
@@ -5,14 +5,17 @@ private:
   vector<bool> internal;
 
 public:
+  // 1.  Here are all the member functions, as directed by the assignment file.
   IntegerSet operator=(const IntegerSet);
+  // 2. I added an overloaded subscript operator, to make my code simpler.
+  bool operator[](int);
   IntegerSet();
   IntegerSet(int[], int);
+  IntegerSet inputSet();
   IntegerSet unionOfSets(IntegerSet);
   IntegerSet intersectionOfSets(IntegerSet);
   bool isEqualTo(IntegerSet);
   void insertElement(int);
   void deleteElement(int);
   void printSet();
-  IntegerSet inputSet();
 };
