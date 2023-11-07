e3096bcbf01fb27fced0b8fa641ed098fc808043
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-11-06 21:03:52 -0800

    started on assignment 9

---
+++ Assignment 9/Aaron_SheyRationalNumber.cpp
@@ -0,0 +1,5 @@
+#include "Aaron_Shey_RationalNumber.h"
+#include <iostream>
+using namespace std;
+
+Polynomial::Polynomial() : numerator(1), denominator(1) {}
---
+++ Assignment 9/Aaron_Shey_RationalNumber.h
@@ -0,0 +1,8 @@
+class RationalNumber {
+private:
+  int numerator, denominator;
+
+public:
+  RationalNumber();
+  void printRational();
+};
